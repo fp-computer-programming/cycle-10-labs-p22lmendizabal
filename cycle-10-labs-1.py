ea5de4979759c06c5eeded6cca3f18203e6843e2
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2022-01-26 10:20:37 -0500

    add file

--- cycle-10-labs-1.py
+++ cycle-10-labs-1.py
@@ -1,1 +1,14 @@
 #author: LM (AMDG) 1/25/22
+from ast import Return
+
+
+def adding_sums(number):
+    number_rangelst = list(range(0, number + 1))
+    for index, value in enumerate(number_rangelst):
+        new_value = value + value[index + 1]
+        del value[index]
+        return new_value
+       
+    
+print(adding_sums(10))
+    
